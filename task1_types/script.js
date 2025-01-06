4123db23b08aa1b53ddbdc0d168839010b756ac5
asaminiko <chebakova29@gmail.com>
2025-01-06 23:50:39 +0200

    2 commit

--- task1_types/script.js
+++ task1_types/script.js
@@ -1,18 +1,22 @@
 const num = 1
 const str = 'java script'
 const bool = true
-
+const sym = Symbol('лалал')
 const n = null
-let bigInt = 0b11111111111111111111111111111111111111111111111111111
-let empty
+let bigInt = BigInt(9007199254740991)
 
 const myArray = [
   [1, 2, 3, 4],
   [5, 6, 7, 8],
 ]
-function lastName(name) {
-  return alert(name)
+const index = myArray[1][2]
+
+const text = 'функція працює'
+function func(props) {
+  console.log(props)
 }
+func(text)
+
 let obj = {
   name: 'Вероніка',
   age: 18,
@@ -20,20 +24,27 @@ let obj = {
   faculty: 'Факультет інформатики та обчислювальної техніки',
   specialty: 'Інформаційні системи та технології',
 }
+console.log(obj) //виводиться початковий обj
 obj = {
   age: 19,
 }
+console.log(obj) //виводиться змінений обj
+
 const obj2 = {
-  // сonst не змінити
   fruit: 'banana',
-  vegitable: 'potato',
+  vegetable: 'potato',
 }
+console.log(obj2) // сonst не змінити
+
 console.log(`${num} типу ${typeof num}`)
 console.log(`${str} типу ${typeof str}`)
+console.log(`${str.length} символів у слові типу ${typeof str.length}`)
 console.log(`${n} типу ${typeof n}`)
-console.log(`${empty} типу ${typeof empty}`)
+console.log(`empty типу ${typeof empty}`)
 console.log(`${bigInt} типу ${typeof bigInt}`)
 console.log(`${bool} типу ${typeof bool}`)
-console.log(`${myArray} типу ${typeof myArray}`)
-console.log(`${lastName('Чебакова')} типу ${typeof lastName}`)
-console.log(`${obj} типу ${typeof obj}`)
+console.log(`sym типу ${typeof sym}`)
+console.log(`myArray типу ${typeof myArray}`)
+console.log(`${index} в масиві типу ${typeof index}`)
+console.log(`obj типу ${typeof obj}`)
+console.log(`func типу ${typeof func}`)
